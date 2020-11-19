2c5e302eabed0c90bdc2b12f444751cd094a8901
s1l3ntq <qimiller89@gmail.com>
2020-11-19 10:53:09 -0600

    commit tree e3169c6956efda1fce03877992f23bb4d041ed12

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+     array.select {|q| q.split("").sort == @word.split("").sort}
+    end
+end   
